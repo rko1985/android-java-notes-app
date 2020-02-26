b53d2d7e0a7d7de48a6a0f775ef0763386d250ab
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-02-26 12:43:39 -1000

    passing note back to main activity

--- app/src/main/java/com/myappcompany/russell/notes/NoteEditorActivity.java
+++ app/src/main/java/com/myappcompany/russell/notes/NoteEditorActivity.java
@@ -3,6 +3,8 @@ package com.myappcompany.russell.notes;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.EditText;
 
 public class NoteEditorActivity extends AppCompatActivity {
@@ -15,10 +17,28 @@ public class NoteEditorActivity extends AppCompatActivity {
         EditText editText = findViewById(R.id.editText);
 
         Intent intent = getIntent();
-        int noteId = intent.getIntExtra("noteId", -1);
+        final int noteId = intent.getIntExtra("noteId", -1);
 
         if(noteId != -1){
             editText.setText(MainActivity.notes.get(noteId));
         }
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                MainActivity.notes.set(noteId, String.valueOf(s));
+                MainActivity.arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 }
