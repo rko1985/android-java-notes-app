3e11cfd780bb0a09048e12607e64fac60df47617
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-02-26 11:18:19 -1000

    creating listview layout

--- app/src/main/java/com/myappcompany/russell/notes/MainActivity.java
+++ app/src/main/java/com/myappcompany/russell/notes/MainActivity.java
@@ -2,12 +2,26 @@ package com.myappcompany.russell.notes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> notes = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.listView);
+        notes.add("Example Note");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notes);
+
+        listView.setAdapter(arrayAdapter);
+
     }
 }
